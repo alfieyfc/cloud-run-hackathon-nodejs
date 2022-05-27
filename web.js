dbce9895fa3b667b3142d6f866ab4c0544727a92
alfieyfc <alfieyfc@gmail.com>
2022-05-27 13:13:07 +0800

    first implemented algo

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+require('dotenv').config();
+
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
@@ -9,10 +11,50 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  console.log(req.body.arena.state);
+  state = req.body.arena.state;
 
-  const moves = ['F', 'T', 'L', 'R'];
-  res.send(moves[1]);
+  let me = state[process.env.MY_URL];
+  let potential_target;
+  let action;
+  for (var key in state) {
+    current_player = state[key]
+    if (key != process.env.MY_URL) {
+
+      // if in the same row and within 3 tiles
+      if (current_player.x == me.x && Math.abs(me.y - current_player.y) < 3) {
+        potential_target = current_player;
+        if (potential_target.x > me.x)
+          if (me.direction == 'E') {
+            action = 'T'
+            break;
+          } else if (me.direction == 'N') {
+            action = 'R'
+          } else {
+            action = 'L'
+          }
+      }
+
+      // if in the same row and within 3 tiles
+      if (current_player.y == me.y && Math.abs(me.x - current_player.x) < 3) {
+        potential_target = current_player
+        if (potential_target.y > me.y)
+          if (me.direction == 'S') {
+            action = 'T'
+            break;
+          } else if (me.direction == 'E') {
+            action = 'R'
+          } else {
+            action = 'L'
+          }
+      }
+    }
+  }
+
+  const moves = ['F', 'L', 'R'];
+  if (!action)
+    action = moves[Math.floor(Math.random() * moves.length)];
+
+  res.send(action);
 });
 
 app.listen(process.env.PORT || 8080);
