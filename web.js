82b41d1208abd097e3dcb792aca40ec9c2534254
alfieyfc <alfieyfc@gmail.com>
2022-05-27 11:51:45 +0800

    log state instad of entire body

--- web.js
+++ web.js
@@ -9,9 +9,16 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  console.log(req.body);
+  // console.log(req.body);
+
+  console.log(req.body.state);
+
   const moves = ['F', 'T', 'L', 'R'];
   res.send(moves[Math.floor(Math.random() * moves.length)]);
 });
+
+app.post('/move', function (req, res) {
+
+})
 
 app.listen(process.env.PORT || 8080);
