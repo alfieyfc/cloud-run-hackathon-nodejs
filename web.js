2fb050729fdba4f7c45ee5d6db7e3e92f8bd6f50
alfieyfc <alfieyfc@gmail.com>
2022-05-27 12:01:26 +0800

    log json of state

--- web.js
+++ web.js
@@ -11,14 +11,10 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   // console.log(req.body);
 
-  console.log(req.body.state);
+  console.log("%j", req.body.state);
 
   const moves = ['F', 'T', 'L', 'R'];
   res.send(moves[0]);
 });
-
-app.post('/move', function (req, res) {
-
-})
 
 app.listen(process.env.PORT || 8080);
