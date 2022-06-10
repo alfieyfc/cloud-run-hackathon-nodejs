a4445be4b6014561c29b7c4703c1de2ccbab77b3
alfieyfc <alfieyfc@gmail.com>
2022-06-10 03:13:53 +0000

    test controlAction

--- util.js
+++ util.js
@@ -1,3 +1,7 @@
+const { move } = require("./web");
+
+const moves = ['T', 'F', 'R', 'L'];
+
 module.exports = {
   topPlayerState: (state) => {
     let topPlayer;
@@ -38,5 +42,119 @@ module.exports = {
     }
     index--
     return probability_array[index]
+  },
+  controlAction: (name, array, dims) => {
+    // control is always at [length-1]
+    var action = moves[Math.floor(Math.random() * moves.length)]
+    var me = array.find(o => o.name == name)
+    if (me.wasHit) {
+      action = 'F'
+      switch (me.direction) {
+        case 'E':
+          if (me.x == dims[0] - 1) {
+            action = 'R'
+            break;
+          }
+          for (var i = 0; i < array.length - 1; i++) {
+            if (array[i].y == me.y && array[i].x - me.x == 1) {
+              action = 'R'
+              break;
+            }
+          }
+          break;
+        case 'W':
+          if (me.x == 0) {
+            action = 'R'
+            break;
+          }
+          for (var i = 0; i < array.length - 1; i++) {
+            if (array[i].y == me.y && array[i].x - me.x == -1) {
+              action = 'R'
+              break;
+            }
+          }
+          break;
+        case 'S':
+          if (me.y == dims[1] - 1) {
+            action = 'R'
+            break;
+          }
+          for (var i = 0; i < array.length - 1; i++) {
+            if (array[i].x == me.x && array[i].y - me.y == 1) {
+              action = 'R'
+              break;
+            }
+          }
+          break;
+        case 'N':
+          if (me.y == 0) {
+            action = 'R'
+            break;
+          }
+          for (var i = 0; i < array.length - 1; i++) {
+            if (array[i].x == me.x && array[i].y - me.y == -1) {
+              action = 'R'
+              break;
+            }
+          }
+          break;
+      }
+    } else {
+      for (var i = 0; i < array.length - 1; i++) {
+        current_player = array[i]
+        if (current_player.name != me.name) {
+          // if in the same col and within 3 tiles
+          if (current_player.x == me.x && Math.abs(me.y - current_player.y) <= 3) {
+            potential_target = current_player;
+            if (potential_target.y > me.y) { // target is on South side
+              if (me.direction == 'S') {
+                action = 'T'
+                break;
+              } else if (me.direction == 'E') {
+                action = 'R'
+              } else {
+                action = 'L'
+              }
+            } else {                           // target is on North side
+              if (me.direction == 'N') {
+                action = 'T'
+                break;
+              } else if (me.direction == 'W') {
+                action = 'R'
+              } else {
+                action = 'L'
+              }
+            }
+          }
+          // if in the same row and within 3 tiles
+          if (current_player.y == me.y && Math.abs(me.x - current_player.x) <= 3) {
+            potential_target = current_player
+            if (potential_target.x > me.x) {  //target is on East side
+              if (me.direction == 'E') {
+                action = 'T'
+                break;
+              } else if (me.direction == 'N') {
+                action = 'R'
+              } else {
+                action = 'L'
+              }
+            } else {                           //target is on West side
+              if (me.direction == 'W') {
+                action = 'T'
+                break;
+              } else if (me.direction == 'S') {
+                action = 'R'
+              } else {
+                action = 'L'
+              }
+            }
+          }
+        }
+      }
+
+    }
+
+    return action
   }
 }
+
--- web.js
+++ web.js
@@ -34,38 +34,7 @@ app.post('/', function (req, res) {
   topPlayer = util.topPlayerState(state);
   closestPlayer = util.closestPlayerState(state, me, myUrl);
 
-  var input = [
-    arenaDims[0],
-    arenaDims[1],
-    numPlayers,
-    me.x,
-    me.y,
-    me.score,
-    directions.indexOf(me.direction),
-    Number(me.wasHit),
-    topPlayer.x,
-    topPlayer.y,
-    topPlayer.score,
-    directions.indexOf(topPlayer.direction),
-    Number(topPlayer.wasHit),
-    closestPlayer.x,
-    closestPlayer.y,
-    closestPlayer.score,
-    directions.indexOf(closestPlayer.direction),
-    Number(closestPlayer.wasHit)
-  ]
-
-  var wIL = Array.from({ length: wILHeight }, () => { return Array.from({ length: wILWidth }, () => { return Math.random() }) });
-  var wLO = Array.from({ length: wLOHeight }, () => { return Array.from({ length: wLOWidth }, () => { return Math.random() }) });
-  var output = math.multiply(math.multiply(input, wIL), wLO)
-  // console.log(output)
-  var output_3 = output.map((e) => { return Math.pow(e, 3) })
-  // console.log(output_3)
-  var sum = output_3.reduce((a, b) => { return a + b })
-  // console.log(sum)
-  var prob = output_3.map((e) => { return e / sum })
-  // console.log(prob)
-  action = moves[prob.indexOf(pickOne(prob))];
+  action = util.controlAction(myUrl, state, arenaDims)
   res.send(action);
 });
 
